11e382548070bef566b479b09e06cb5400a79899
Sam <samthui7@gmail.com>
2022-04-30 22:12:46 +0700

    Init basic UI

---
+++ Carro/Carro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Carro
+//
+//  Created by N T P Ngoc on 28/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
