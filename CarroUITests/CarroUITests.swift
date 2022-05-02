aa06126c206c50fdd77ba2c2348cf77732d8c9e4
Sam <samthui7@gmail.com>
2022-05-02 17:12:48 +0700

    UI tests

--- CarroUITests/CarroUITests.swift
+++ CarroUITests/CarroUITests.swift
@@ -7,28 +7,72 @@
 
 import XCTest
 
+enum Country: String, Codable, CaseIterable {
+    case Singapore, Thailand
+}
+
 class CarroUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app.terminate()
+        app = nil
+    }
+    
+    // TODO: failed related to RadioGroup
+    func selectCountry(_ country: String) {
+        let predicate = NSPredicate(format: "label CONTAINS '\(country)'")
+        app.staticTexts.element(matching: predicate).tap()
+    }
+    
+    func expectItem(_ item: String, forCountry country: String, _ exist: Bool) {
+        if exist {
+            XCTAssertTrue(app.staticTexts[item].exists, "show '\(item)' for \(country)")
+        } else {
+            XCTAssertFalse(app.staticTexts[item].exists, "hide '\(item)' for \(country)")
+        }
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDisplayDataSingapore() throws {
+        // TODO: failed related to RadioGroup
+//        XCTAssert(app.staticTexts["Singapore"].exists)
+//        XCTAssert(app.staticTexts["Thailand"].exists)
+        
+        let nextButton = app.buttons["Next"]
+        XCTAssert(nextButton.exists)
+        nextButton.tap()
+        
+        expectItem("Usage Based Insurance", forCountry: Country.Singapore.rawValue, true)
+        expectItem("Name Driver", forCountry: Country.Singapore.rawValue, true)
+        expectItem("Customize your Insurance", forCountry: Country.Singapore.rawValue, true)
+        expectItem("View Docs", forCountry: Country.Singapore.rawValue, true)
+        expectItem("Total Fines", forCountry: Country.Thailand.rawValue, false)
+        expectItem("Total Fines Amount", forCountry: Country.Thailand.rawValue, false)
+        
+        app.navigationBars.buttons["Select Country"].tap()
     }
+    
+    // TODO: failed related to RadioGroup
+//    func testDisplayDataThailand() throws {
+//        selectCountry("Thailand")
+//        
+//        let nextButton = app.buttons["Next"]
+//        XCTAssert(nextButton.exists)
+//        nextButton.tap()
+//        
+//        expectItem("Usage Based Insurance", forCountry: Country.Thailand.rawValue, false)
+//        expectItem("Name Driver", forCountry: Country.Thailand.rawValue, false)
+//        expectItem("Customize your Insurance", forCountry: Country.Thailand.rawValue, false)
+//        expectItem("View Docs", forCountry: Country.Thailand.rawValue, false)
+//        expectItem("Total Fines", forCountry: Country.Singapore.rawValue, true)
+//        expectItem("Total Fines Amount", forCountry: Country.Singapore.rawValue, true)
+//    }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
