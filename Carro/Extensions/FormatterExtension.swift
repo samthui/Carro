9f8a4b03f99aa37cc46e699851640172955b0303
Sam <samthui7@gmail.com>
2022-05-02 13:35:54 +0700

    CalendarExtensions tests

--- Carro/Extensions/FormatterExtension.swift
+++ Carro/Extensions/FormatterExtension.swift
@@ -8,17 +8,6 @@
 import Foundation
 
 extension NumberFormatter {
-    static func formatCurrency(_ value: Double) -> String! {
-        let formatter = NumberFormatter()
-        formatter.currencySymbol = Constants.kCurrencySymbol
-        formatter.usesGroupingSeparator = true
-        formatter.numberStyle = .currency
-        formatter.locale = Locale.current
-
-        let number = NSNumber(value: value)
-        return formatter.string(from: number)!
-    }
-    
     func formatCurrency(_ value: Double) -> String! {
         self.currencySymbol = Constants.kCurrencySymbol
         self.usesGroupingSeparator = true
---
+++ CarroTests/ExtensionsTests/CalendarExtensionTests.swift
@@ -0,0 +1,39 @@
+//
+//  CalendarExtensionTests.swift
+//  Carro
+//
+//  Created by N T P Ngoc on 02/05/2022.
+//
+
+import XCTest
+@testable import Carro
+
+class CalendarExtensionTests: XCTestCase {
+    private var calendar: Calendar!
+    let secondsPerDay = 86400.0
+
+    override func setUpWithError() throws {
+        calendar = Calendar.current
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testNumberOfDaysBetweenTimestamp() throws {
+        let diff = 7.0
+        
+        let timestamp1 = 12.0
+        let timestamp2 = timestamp1 + diff*secondsPerDay
+        
+        XCTAssertEqual(calendar.numberOfDaysBetweenTimestamp(timestamp1, and: timestamp2), Int(diff) + 1)
+    }
+    
+    func testNumberOfDaysBetweenDates() throws {
+        let diff = 5.0
+        
+        let date1 = Date()
+        let date2 = date1.addingTimeInterval(diff*secondsPerDay)
+        
+        XCTAssertEqual(calendar.numberOfDaysBetweenDates(date1, and: date2), Int(diff) + 1)
+    }
+}
